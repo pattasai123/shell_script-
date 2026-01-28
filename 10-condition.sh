0c646c429a8373d923a52b1de5ff3e7304688f6d
balaji <balubongu185@gmail.com>
2026-01-29 01:23:31 +0530

    condition

--- 10-condition.sh
+++ 10-condition.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 number=$1
-val=$((number%2))
-if [$val -eq 0]; then
-    echo "this is a even number"
+val=$((number % 2))
+
+if [ $val -eq 0 ]; then
+    echo "this is an even number"
 else 
     echo "this is an odd number"
 fi
