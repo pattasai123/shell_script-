e1dab821c5d0cb435816fb27b935fa4b2e9691dd
balaji <balubongu185@gmail.com>
2026-01-29 14:23:28 +0530

    exitstatus

--- 11-exitstatus.sh
+++ 11-exitstatus.sh
@@ -6,8 +6,17 @@ if [ $user -ne 0 ]; then
     echo "ERROR:: please run this with root access"
 fi
 
-if dnf install nginx -y; then 
-    echo "Success"
+# if dnf install nginx -y; then 
+#     echo "Success"
+# else
+#     echo "failure"
+# fi
+
+dnf install mysql -y
+
+query=$?
+if [ $query -eq 0 ]; then 
+    echo "success"
 else
     echo "failure"
 fi
