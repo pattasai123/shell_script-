bc969e48c23f8bafad0f5c198dae9d867280aa8d
balaji <balubongu185@gmail.com>
2026-02-20 15:52:41 +0530

    deletion of file or log

--- deletefile.sh
+++ deletefile.sh
@@ -7,12 +7,44 @@ if [ dir -ne 0 ]; then
     echo -e "$r please access this file with root directory $n"
     exit 1;
 fi
-val=5
-while [ $val -gt 0 ]
-do 
-    echo "$val"
-    val=$(($val-1))
-    sleep 1;
-done
-echo "$val"
-echo "while loop is completed"
+source=$1
+destination=$2
+if [ $* -lt 2 ]
+    usage
+fi
+
+usage(){
+    echo -e "$r use the command in a proper way $n"
+}
+
+if [ ! -d $source ]; then
+    echo -e "$r file is not exist in the source directory!!"
+    exit 1;
+fi
+
+if [ ! -d $destination ]; then
+    echo -e "$r file is not exist in the destination directory!!"
+    exit 1;
+fi
+
+files=$(find $source -name "*.log" -type f)
+
+if [ ! -z "{$files}" ]; then
+    echo "file exit"
+    date=$(date)
+    filename="$destination/applogs-$date.zip"
+    $files | zip -@ -j "$filename"
+    if [ -f $filename ]; then
+        echo "$g success... $n"
+        while IFS= read -r filepath
+        do 
+            rm -ff $filepath
+            echo "Deleting $filepath"
+        done
+    else
+        echo "Archieval ... $R FAILURE $N"
+        exit 1
+    fi
+else
+    echo -e "No files to archeive ... $Y SKIPPING $N"
+fi
