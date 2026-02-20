8b6819b163ba41bc9db8bc22d3669d5239872710
balaji <balubongu185@gmail.com>
2026-02-20 14:58:55 +0530

    deletion of file or log

---
+++ deletefile.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+r="\e[31m"
+g='\e[32m'
+n='\e[33m'
+dir=$(id -u)
+if [ dir -ne 0 ]; then
+    echo -e "$r please access this file with root directory $n"
+    exit 1;
+fi
+val=5
+while 5 -gt 0
+do 
+    echo "$val"
+    val=$(($val-1))
+    sleep 1;
+done
+echo "$val"
+echo "while loop is completed"
