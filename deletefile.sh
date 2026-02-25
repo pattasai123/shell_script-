de1e6bff75172f5980bd8843b9ea6471e97b1644
balaji <balubongu185@gmail.com>
2026-02-25 12:08:43 +0530

    deletefile

--- deletefile.sh
+++ deletefile.sh
@@ -7,9 +7,11 @@ if [ $dir -ne 0 ]; then
     echo -e "$r please access this file with root directory $n"
     exit 1;
 fi
+
 source=$1
 destination=$2
-if [ $@ -ne 2 ]; then
+
+if [ $# -ne 2 ]; then
     usage
 fi
 
@@ -17,28 +19,28 @@ usage(){
     echo -e "$r use the command in a proper way $n"
 }
 
-if [ ! -d $source ]; then
+if [ ! -d "$source" ]; then
     echo -e "$r file is not exist in the source directory!!"
     exit 1;
 fi
 
-if [ ! -d $destination ]; then
+if [ ! -d "$destination" ]; then
     echo -e "$r file is not exist in the destination directory!!"
     exit 1;
 fi
 
-files=$(find $source -name "*.log" -type f)
+files=$(find "$source" -name "*.log" -type f)
 
 if [ ! -z "{$files}" ]; then
     echo "file exit"
     date=$(date)
     filename="$destination/applogs-$date.zip"
-    $files | zip -@ -j "$filename"
+    find "$source" -name "*.log" -type f | zip -@ -j "$filename"
     if [ -f $filename ]; then
         echo "$g success... $n"
-        while IFS= read -r filepath
+        find "$source" -name "*.log" -type f | while IFS= read -r filepath
         do 
-            rm -ff $filepath
+            rm -f $filepath
             echo "Deleting $filepath"
         done
     else
