a9cbe6124c45ae91beb43ab865273fc61c45d08d
balaji <balubongu185@gmail.com>
2026-03-17 12:19:15 +0530

    deletion of file or log

--- deletefile.sh
+++ deletefile.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 r="\e[31m"
 g='\e[32m'
-n='\e[33m'
+n='\e[0m'
 dir=$(id -u)
 if [ $dir -ne 0 ]; then
-    echo -e "$r please access this file with root directory $n"
+    echo -e "${r} please access this file with root directory ${n}"
     exit 1;
 fi
 
@@ -16,16 +16,16 @@ if [ $# -ne 2 ]; then
 fi
 
 usage(){
-    echo -e "$r use the command in a proper way $n"
+    echo -e "${r} use the command in a proper way ${n}"
 }
 
 if [ ! -d "$source" ]; then
-    echo -e "$r file is not exist in the source directory!!"
+    echo -e "${r} file is not exist in the source directory!!"
     exit 1;
 fi
 
 if [ ! -d "$destination" ]; then
-    echo -e "$r file is not exist in the destination directory!!"
+    echo -e "${r} file is not exist in the destination directory!!"
     exit 1;
 fi
 
@@ -37,16 +37,16 @@ if [ ! -z "$files" ]; then
     filename="$destination/applogs-$date.zip"
     find "$source" -name "*.log" -type f | zip -@ -j "$filename"
     if [ -f "$filename" ]; then
-        echo "$g success... $n"
+        echo "${g} success... ${n}"
         find "$source" -name "*.log" -type f | while IFS= read -r filepath
         do 
             rm -f $filepath
             echo "Deleting $filepath"
         done
     else
-        echo "Archieval ... $r FAILURE $n"
+        echo "Archieval ... ${r} FAILURE ${n}"
         exit 1
     fi
 else
-    echo -e "No files to archeive ... $r SKIPPING $n"
+    echo -e "No files to archeive ... ${r} SKIPPING ${n}"
 fi
